f28b671b610c2680b2b530a7ffcb35e9a2e2bbb9
xieby1 <xieby1@outlook.com>
2024-12-09 13:50:04 +0800

    nix: enable ccache

--- shell.nix
+++ shell.nix
@@ -7,15 +7,42 @@ let
     rev = "190c31a89e5eec80dd6604d7f9e5af3802a58a13";
     hash = "sha256-K5DJ2LpPqht7K76bsxetI+YHhGGRyVteTPRQaIIKJpw=";
   }) {};
-in pkgs.mkShell {
+  ccache_dir = toString ./. + "/.ccache";
+  ccache14Stdenv = pkgs.ccacheStdenv.override {
+    stdenv = pkgs.gcc14Stdenv;
+    extraConfig = ''
+      export CCACHE_COMPRESS=1
+      export CCACHE_DIR="${ccache_dir}"
+      export CCACHE_UMASK=007
+      if [ ! -d "$CCACHE_DIR" ]; then
+        echo "====="
+        echo "Directory '$CCACHE_DIR' does not exist"
+        echo "Please create it with:"
+        echo "  mkdir -m0770 '$CCACHE_DIR'"
+        echo "====="
+        exit 1
+      fi
+      if [ ! -w "$CCACHE_DIR" ]; then
+        echo "====="
+        echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
+        echo "Please verify its access permissions"
+        echo "====="
+        exit 1
+      fi
+    '';
+  };
+  ccacheMkShell = pkgs.mkShell.override {
+    stdenv = ccache14Stdenv;
+  };
+in ccacheMkShell {
   inherit name;
 
   buildInputs = let
     h_content = builtins.toFile "h_content" ''
       # ${pkgs.lib.toUpper "${name} usage tips"}
 
-      * make init
-      * make emu -j`nproc`
+      * `make init`
+      * ``make emu -j`nproc` ``
     '';
     _h_ = pkgs.writeShellScriptBin "h" ''
       ${pkgs.glow}/bin/glow ${h_content}
@@ -49,5 +76,6 @@ in pkgs.mkShell {
     h
     export CHISEL_FIRTOOL_PATH=${circt_1_62_0}/bin/
     export NOOP_HOME=$(realpath .)
+    mkdir -m0770 -p ${ccache_dir}
   '';
 }

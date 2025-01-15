38e15e86326e97af0013961c76ba5e63d7f6dff5
fawangZ <fwcheung@outlook.com>
2025-01-15 13:20:56 +0800

    area(bpu): reduce meta

--- src/main/scala/xiangshan/frontend/BPU.scala
+++ src/main/scala/xiangshan/frontend/BPU.scala
@@ -28,7 +28,7 @@ import scala.math.min
 import xiangshan.backend.decode.ImmUnion
 
 trait HasBPUConst extends HasXSParameter {
-  val MaxMetaBaseLength =  if (!env.FPGAPlatform) 512 else 148 // TODO: Reduce meta length
+  val MaxMetaBaseLength =  if (!env.FPGAPlatform) 512 else 96 // TODO: Reduce meta length
   val MaxMetaLength = if (HasHExtension) MaxMetaBaseLength + 4 else MaxMetaBaseLength  // 64X152 sram is batter than 64X150
   val MaxBasicBlockSize = 32
   val LHistoryLength = 32

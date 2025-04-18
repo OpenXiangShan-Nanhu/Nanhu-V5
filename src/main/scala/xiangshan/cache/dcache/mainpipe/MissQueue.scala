6fd9e349874691443c1c471a829fe973d9d0d35b
Fawang <fwcheung@outlook.com>
2025-04-18 10:33:36 +0800

    Revert "fix: probe request is processed early and cause errors"
    This reverts commit 2ad86cf97471fb05d752719031a60d4fbc211472.
    
    fix: DCache MissQueue

--- src/main/scala/xiangshan/cache/dcache/mainpipe/MissQueue.scala
+++ src/main/scala/xiangshan/cache/dcache/mainpipe/MissQueue.scala
@@ -699,7 +699,7 @@ class MissEntry(edge: TLEdgeOut, reqNum: Int)(implicit p: Parameters) extends DC
 
   val grantack = RegEnable(edge.GrantAck(io.mem_grant.bits), io.mem_grant.fire)
   assert(RegNext(!io.mem_grant.fire || edge.isRequest(io.mem_grant.bits)))
-  io.mem_finish.valid := !s_grantack && w_grantfirst
+  io.mem_finish.valid := !s_grantack && w_grantlast
   io.mem_finish.bits := grantack
 
   // Send mainpipe_req when receive hint from L2 or receive data without hint
@@ -719,7 +719,7 @@ class MissEntry(edge: TLEdgeOut, reqNum: Int)(implicit p: Parameters) extends DC
   io.main_pipe_req.bits.pf_source := req.pf_source
   io.main_pipe_req.bits.access := access
 
-  io.block_addr.valid := req_valid
+  io.block_addr.valid := req_valid && w_grantlast 
   io.block_addr.bits := req.addr
 
   io.req_addr.valid := req_valid

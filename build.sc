emeraldsoft3/XiangShan

build: add `difftest.test` to build target (#4135)
Needed by verification team.

build.sc
@@ -175,6 +175,12 @@ object difftest extends HasChisel {
 
   override def millSourcePath = pwd / "difftest"
 
+  object test extends SbtTests with TestModule.ScalaTest {
+    override def sources = T.sources {
+      super.sources() ++ Seq(PathRef(millSourcePath / "src" / "generator" / "chisel"))
+    }
+  }
+
 }
 
 object fudian extends HasChisel {
@@ -331,6 +337,10 @@ object xiangshan extends XiangShanModule with HasChisel with ScalafmtModule {
   }
 
   object test extends SbtTests with TestModule.ScalaTest {
+    override def moduleDeps = super.moduleDeps ++ Seq(
+      difftestModule.test
+    )
+
     override def forkArgs = forkArgsTask()
 
     override def ivyDeps = super.ivyDeps() ++ Agg(

OpenXiangShan-Nanhu/Nanhu-V5

build.sc
@@ -187,6 +187,12 @@ object difftest extends HasChisel {
 
   override def millSourcePath = os.pwd / "difftest"
 
+  object test extends SbtModuleTests with TestModule.ScalaTest {
+    override def sources = T.sources {
+      super.sources() ++ Seq(PathRef(millSourcePath / "src" / "generator" / "chisel"))
+    }
+  }
+
 }
 
 object fudian extends HasChisel {
@@ -345,6 +351,9 @@ object xiangshan extends XiangShanModule with HasChisel {
 
   object test extends SbtModuleTests with TestModule.ScalaTest {
     override def forkArgs = Seq("-Xmx40G", "-Xss256m")
+    override def moduleDeps = super.moduleDeps ++ Seq(
+      difftestModule.test
+    )
 
     override def ivyDeps = super.ivyDeps() ++ Agg(
       defaultVersions("chiseltest")
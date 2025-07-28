61cd352c2b2ca29925821b3326bcbb758b93eff2
linmingfeng <1550263950@qq.com>
2025-07-28 12:22:58 +0800

    refactor(UnCache): Add same-address restrictions

--- src/main/scala/xiangshan/cache/dcache/Uncache.scala
+++ src/main/scala/xiangshan/cache/dcache/Uncache.scala
@@ -28,162 +28,18 @@ import freechips.rocketchip.tilelink.{TLArbiter, TLBundleA, TLBundleD, TLClientN
 import xs.utils.cache.{MemBackTypeMM, MemBackTypeMMField, MemPageTypeNC, MemPageTypeNCField}
 import difftest._
 
-class UncachePtr(implicit p: Parameters) extends CircularQueuePtr[UncachePtr](
-  p => p(XSCoreParamsKey).UncacheBufferSize
-){
-
-}
-
-object UncachePtr {
-  def apply(f: Bool, v: UInt)(implicit p: Parameters): UncachePtr = {
-    val ptr = Wire(new UncachePtr)
-    ptr.flag := f
-    ptr.value := v
-    ptr
-  }
-}
 
 class UncacheFlushBundle extends Bundle {
   val valid = Output(Bool())
   val empty = Input(Bool())
 }
 
-// One miss entry deals with one mmio request
-class MMIOEntry(edge: TLEdgeOut)(implicit p: Parameters) extends DCacheModule
-{
-  val io = IO(new Bundle {
-    //  MSHR ID
-    val hartId = Input(UInt())
-    //  Client requests
-    val req = Flipped(DecoupledIO(new UncacheWordReq))
-    val resp = DecoupledIO(new DCacheWordRespWithError)
-
-    //  TileLink
-    val mem_acquire = DecoupledIO(new TLBundleA(edge.bundle))
-    val mem_grant = Flipped(DecoupledIO(new TLBundleD(edge.bundle)))
-
-    // This entry is valid.
-    val invalid = Output(Bool())
-    //  This entry is selected.
-    val select = Input(Bool())
-  })
-  //  ================================================
-  //  FSM state description:
-  //  s_invalid     : Entry is invalid.
-  //  s_refill_req  : Send Acquire request.
-  //  s_refill_resp : Wait for Grant response.
-  //  s_send_resp   : Send Uncache response.
-  val s_invalid :: s_refill_req :: s_refill_resp :: s_send_resp :: Nil = Enum(4)
-  val state = RegInit(s_invalid)
-
-  val req = Reg(new UncacheWordReq)
-  val resp_data = Reg(UInt(DataBits.W))
-  val resp_nderr = Reg(Bool())
-  def storeReq = req.cmd === MemoryOpConstants.M_XWR
-
-  io.invalid := state === s_invalid
-  //  Assign default values to output signals.
-  io.req.ready := false.B
-  io.resp.valid := false.B
-  io.resp.bits := DontCare
-  io.resp.bits.isStore := req.cmd === MemoryOpConstants.M_XWR
-
-  io.mem_acquire.valid := false.B
-  io.mem_acquire.bits := DontCare
-  io.mem_grant.ready := false.B
-
-  //  Receive request
-  when (state === s_invalid) {
-    io.req.ready := true.B
-
-    when (io.req.fire) {
-      req := io.req.bits
-      req.addr := io.req.bits.addr
-      resp_nderr := false.B
-      state := s_refill_req
-    }
-  }
-
-  //  Refill
-  //  TODO: determine 'lgSize' in memend
-  val size = PopCount(req.mask)
-  val (lgSize, legal) = PriorityMuxWithFlag(Seq(
-    1.U -> 0.U,
-    2.U -> 1.U,
-    4.U -> 2.U,
-    8.U -> 3.U
-  ).map(m => (size===m._1) -> m._2))
-  assert(!(io.mem_acquire.valid && !legal))
-
-  val load = edge.Get(
-    fromSource      = io.hartId,
-    toAddress       = req.addr,
-    lgSize          = lgSize
-  )._2
-
-  val store = edge.Put(
-    fromSource      = io.hartId,
-    toAddress       = req.addr,
-    lgSize          = lgSize,
-    data            = req.data,
-    mask            = req.mask
-  )._2
-
-  XSDebug("entry: %d state: %d\n", io.hartId, state)
-
-  when (state === s_refill_req) {
-    io.mem_acquire.valid := true.B && io.select
-    io.mem_acquire.bits := Mux(storeReq, store, load)
-    io.mem_acquire.bits.user.lift(MemBackTypeMM).foreach(_ := req.nc)
-    io.mem_acquire.bits.user.lift(MemPageTypeNC).foreach(_ := req.nc)
-    when (io.mem_acquire.fire) {
-      state := s_refill_resp
-    }
-  }
-
-  val (_, _, refill_done, _) = edge.addr_inc(io.mem_grant)
-  when (state === s_refill_resp) {
-    io.mem_grant.ready := true.B
-
-    when (io.mem_grant.fire) {
-      resp_data := io.mem_grant.bits.data
-      resp_nderr := io.mem_grant.bits.denied
-      io.resp.valid := req.nc && storeReq
-      io.resp.bits.isNC  := req.nc
-      assert(refill_done, "Uncache response should be one beat only!")
-      state := Mux(storeReq && req.nc, s_invalid, s_send_resp)
-    }
-  }
-
-  //  Response
-  when (state === s_send_resp) {
-    io.resp.valid := true.B
-    io.resp.bits.data   := resp_data
-    // meta data should go with the response
-    io.resp.bits.id     := req.id
-    io.resp.bits.miss   := false.B
-    io.resp.bits.replay := false.B
-    io.resp.bits.tag_error := false.B
-    io.resp.bits.error := false.B
-    io.resp.bits.nderr := resp_nderr
-    io.resp.bits.isNC  := req.nc
-
-    when (io.resp.fire) {
-      state := s_invalid
-    }
-  }
-
-  //  End
-}
 
 class UncacheIO(implicit p: Parameters) extends DCacheBundle {
   val hartId = Input(UInt())
   val flush = Flipped(new UncacheFlushBundle)
   val lsq = Flipped(new UncacheWordIO)
 }
-
-// convert DCacheIO to TileLink
-// for Now, we only deal with TL-UL
 
 class Uncache()(implicit p: Parameters) extends LazyModule with HasXSParameter {
   override def shouldBeInlined: Boolean = false
@@ -209,79 +65,58 @@ class UncacheImp(outer: Uncache)extends LazyModuleImp(outer)
  val io = IO(new UncacheIO)
 
   val (bus, edge) = outer.clientNode.out.head
-
-  val req  = io.lsq.req
-  val resp = io.lsq.resp
   val mem_acquire = bus.a
   val mem_grant   = bus.d
-
-  val req_ready = WireInit(false.B)
 
   // assign default values to output signals
   bus.b.ready := false.B
   bus.c.valid := false.B
   bus.c.bits  := DontCare
-  bus.d.ready := false.B
+  bus.d.ready := true.B
   bus.e.valid := false.B
   bus.e.bits  := DontCare
 
-  val enqPtr = RegInit(0.U.asTypeOf(new UncachePtr))
-  val issPtr = RegInit(0.U.asTypeOf(new UncachePtr))
-  val deqPtr = RegInit(0.U.asTypeOf(new UncachePtr))
-
-  io.lsq.resp.valid := false.B
-  io.lsq.resp.bits := DontCare
-
-  val entries = Seq.fill(UncacheBufferSize) { Module(new MMIOEntry(edge)) }
-  for ((entry, i) <- entries.zipWithIndex) {
-    entry.io.hartId := io.hartId
-
-   //  Enqueue
-    entry.io.req.valid := (i.U === enqPtr.value) && req.valid
-    entry.io.req.bits := req.bits
-
-    when (i.U === enqPtr.value) {
-      req_ready := entry.io.req.ready
-    }
-
-    //  Acquire
-    entry.io.select := (i.U === issPtr.value)
-
-    //  Grant
-    entry.io.mem_grant.valid := false.B
-    entry.io.mem_grant.bits := DontCare
-    when (i.U === deqPtr.value) {
-      entry.io.mem_grant <> mem_grant
-    }
-
-    entry.io.resp.ready := false.B
-    when (i.U === deqPtr.value) {
-      io.lsq.resp <> entry.io.resp
-    }
-
+  def getFirstOneOH(input: UInt): UInt = {
+    assert(input.getWidth > 1)
+    val output = WireInit(VecInit(input.asBools))
+    (1 until input.getWidth).map(i => {
+      output(i) := !input(i - 1, 0).orR && input(i)
+    })
+    output.asUInt
   }
 
-  io.lsq.req.ready := req_ready
+  val entries = Seq.fill(UncacheBufferSize) { Module(new MMIOEntry(edge)) }
+  val acquireArbiter = Module(new Arbiter(new TLBundleA(edge.bundle), UncacheBufferSize))
 
-    //  Enqueue
-    when (req.fire) {
-      enqPtr := enqPtr + 1.U
-    }
+  val entriesReadyVec = entries.map(_.io.req.ready)
+  val lsqReqEntrySel = getFirstOneOH(VecInit(entriesReadyVec).asUInt)
 
-    //  Issue
-    when (mem_acquire.fire) {
-      issPtr := issPtr + 1.U
-    }
+  val enqHasSamePA = io.lsq.req.valid && entries.map({ case e =>
+    e.io.entryInfo.valid && (e.io.entryInfo.paddr(PAddrBits - 1, 3) === io.lsq.req.bits.addr(PAddrBits - 1, 3))
+  }).reduce(_|_)
 
-    when (io.lsq.resp.fire) {
-      deqPtr := deqPtr + 1.U
-    }
+  io.lsq.req.ready := entriesReadyVec.reduce(_|_) && !enqHasSamePA
 
-  TLArbiter.lowestFromSeq(edge, mem_acquire, entries.map(_.io.mem_acquire))
-  val invalid_entries = PopCount(entries.map(_.io.invalid))
-  io.flush.empty := invalid_entries === UncacheBufferSize.U
+  for ((entry, i) <- entries.zipWithIndex) {
+    entry.io.sourceId := i.U
+    entry.io.mem_grant.valid := mem_grant.valid && (mem_grant.bits.source === i.U)
+    entry.io.mem_grant.bits <> mem_grant.bits
 
-  println(s"Uncahe Buffer Size: $UncacheBufferSize entries")
+    entry.io.req.valid := io.lsq.req.valid && lsqReqEntrySel(i)
+    entry.io.req.bits := io.lsq.req.bits
+
+    entry.io.resp.ready := true.B
+
+    acquireArbiter.io.in(i) <> entry.io.mem_acquire
+  }
+
+  mem_acquire <> acquireArbiter.io.out
+
+  val lsqRespSeq = entries.map(_.io.resp)
+  io.lsq.resp.valid := lsqRespSeq.map(_.valid).reduce(_|_)
+  io.lsq.resp.bits := PriorityMux(lsqRespSeq.map(_.valid),lsqRespSeq.map(_.bits))
+
+  io.flush.empty := !entries.map(_.io.entryInfo.valid).reduce(_|_)
 
   // uncache store but memBackTypeMM should update the golden memory
   if (env.EnableDifftest) {
@@ -294,23 +129,15 @@ class UncacheImp(outer: Uncache)extends LazyModuleImp(outer)
     difftest.mask   := mem_acquire.bits.mask
   }
 
-  // print all input/output requests for debug purpose
-  // print req/resp
-  XSDebug(req.fire, "req cmd: %x addr: %x data: %x mask: %x\n",
-    req.bits.cmd, req.bits.addr, req.bits.data, req.bits.mask)
-  XSDebug(resp.fire, "data: %x\n", req.bits.data)
 
-  //  Performance Counters
+  println(s"Uncahe Buffer Size: $UncacheBufferSize entries")
   def isStore: Bool = io.lsq.req.bits.cmd === MemoryOpConstants.M_XWR
   XSPerfAccumulate("mmio_store", io.lsq.req.fire && isStore)
   XSPerfAccumulate("mmio_load", io.lsq.req.fire && !isStore)
-  XSPerfAccumulate("mmio_outstanding", mem_acquire.fire && (deqPtr =/= issPtr))
   val perfEvents = Seq(
     ("mmio_store", io.lsq.req.fire && isStore),
     ("mmio_load", io.lsq.req.fire && !isStore),
-    ("mmio_outstanding", mem_acquire.fire && (deqPtr =/= issPtr))
   )
 
   generatePerfEvent()
-  //  End
 }

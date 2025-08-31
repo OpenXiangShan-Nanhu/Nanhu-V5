6e7c516aa68bf4417bb25cb9d82e039603e1255f
linmingfeng <1550263950@qq.com>
2025-08-31 17:48:16 +0800

    fix(DCache): restore l2 hint

--- src/main/scala/xiangshan/cache/dcache/DCacheWrapper.scala
+++ src/main/scala/xiangshan/cache/dcache/DCacheWrapper.scala
@@ -1001,7 +1001,6 @@ class DCacheImp(outer: DCache) extends LazyModuleImp(outer) with HasDCacheParame
   missQueue.io.l2_pf_store_only := RegNext(io.l2_pf_store_only, false.B)
   missQueue.io.debugTopDown <> io.debugTopDown
   missQueue.io.l2_hint <> RegNext(io.l2_hint)
-  missQueue.io.l2_hint <> DontCare
   missQueue.io.mainpipe_info := mainPipe.io.mainpipe_info
   mainPipe.io.refill_info := missQueue.io.refill_info
   mainPipe.io.replace <> missQueue.io.replace_block_query

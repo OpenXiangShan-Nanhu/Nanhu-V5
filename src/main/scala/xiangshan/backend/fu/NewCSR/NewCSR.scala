2d5fea9a4d045adcad3aa63bf64ce5067ae02880
zhangbin <869284530@qq.com>
2025-10-10 09:47:30 +0800

    fix(nmi): fix ecc error occur of uncache & fix the logic for gating nmi

--- src/main/scala/xiangshan/backend/fu/NewCSR/NewCSR.scala
+++ src/main/scala/xiangshan/backend/fu/NewCSR/NewCSR.scala
@@ -372,10 +372,10 @@ class NewCSR(implicit val p: Parameters) extends Module
 
   val intrVec = RegEnable(intrMod.io.out.interruptVec.bits, 0.U, intrMod.io.out.interruptVec.valid)
   val debug = RegEnable(intrMod.io.out.debug, false.B, intrMod.io.out.interruptVec.valid)
-  val nmi = RegEnable(intrMod.io.out.nmi, false.B, intrMod.io.out.interruptVec.valid)
   val virtualInterruptIsHvictlInject = RegEnable(intrMod.io.out.virtualInterruptIsHvictlInject, false.B, intrMod.io.out.interruptVec.valid)
   val irToHS = RegEnable(intrMod.io.out.irToHS, false.B, intrMod.io.out.interruptVec.valid)
   val irToVS = RegEnable(intrMod.io.out.irToVS, false.B, intrMod.io.out.interruptVec.valid)
+  val nmi = intrMod.io.out.nmi
 
   when(hasTrap && trapIsInterrupt && nmi) {
     nmip.NMI_31 := nmip.NMI_31 & (!intrVec(NonMaskableIRNO.NMI_31) | intrVec(NonMaskableIRNO.NMI_43))
